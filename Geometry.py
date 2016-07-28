d38a186ebdacfd51aabe7e68b9d44481c2dfbe5f
Ava Jetli <avajetli@umich.edu>
2016-07-28 12:06:33 -0400

    removed the simple geometry portion of the code in compartment geometry

--- Geometry.py
+++ Geometry.py
@@ -68,23 +68,7 @@ def viewCompartmentGeo(G):
 	for node in G.nodes():
 		if 'Type' in G.node[node]:
 			if G.node[node]['Type'] == 'Compartment':
-				try: # simple geometry
-					# if geometry is simple then graph one cube with centroid x,y,z and edge length a
-					int(G.node[node]['x'])
-					a = G.node[node]['EdgeLength']
-					x = G.node[node]['x']
-					y = G.node[node]['y']
-					z = G.node[node]['z']
-					hSL = float(a/2)
-					r = [-hSL, hSL]
-					rX = [-hSL + x, hSL + x]
-					rY = [-hSL + y, hSL + y]
-					rZ = [-hSL + z, hSL + z]
-					for s, e in combinations(np.array(list(product(rX,rY,rZ))), 2):
-						if not np.sum(np.abs(s-e)) > a+0.0000001:
-							ax.plot3D(*zip(s,e), color="b")
-				except TypeError: #advanced geometry
-					# if geometry is advanced, loop through each sub geometry and plot the cube with centroid x,y,z and edge length a
+					#loop through each sub geometry and plot the cube with centroid x,y,z and edge length a
 					voxelHandler = vox.voxelHandler()
 					cubePos = []
 					for i in range(0, len(G.node[node]['x'])):	
